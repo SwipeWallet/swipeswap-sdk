97fd9324fe106b0712264f823ea9f3d5ee704472
Crypto Pro <cryptopro@C02DQ3NRMD6P.local>
2021-06-07 00:47:50 +1000

    Update for swipe swap

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
 import JSBI from 'jsbi'
 export { JSBI }
 
-// export {
-//   BigintIsh,
-//   ChainId,
-//   TradeType,
-//   Rounding,
-//   FACTORY_ADDRESS,
-//   INIT_CODE_HASH,
-//   ROUTER_ADDRESS,
-//   SUSHI_ADDRESS,
-//   MASTERCHEF_ADDRESS,
-//   BAR_ADDRESS,
-//   MAKER_ADDRESS,
-//   TIMELOCK_ADDRESS,
-//   MINIMUM_LIQUIDITY
-// } from './constants'
-
 export * from './constants'
 
 export * from './errors'
